7f71e7fb9000b56c05857b45b766c48f5b470305
Francisco Thiago <fthiagomedeiros@gmail.com>
2021-10-27 19:53:55 +0100

    deploying settings in ElasticBeanstalk

--- src/main/java/com/cocus/challenge/fizzbuzz/controllers/FizzBuzzController.java
+++ src/main/java/com/cocus/challenge/fizzbuzz/controllers/FizzBuzzController.java
@@ -6,6 +6,7 @@ import com.cocus.challenge.fizzbuzz.service.FizzBuzzService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,12 @@ public class FizzBuzzController {
     private final static String PATH = "/fizzbuzz";
 
     private final FizzBuzzService service;
+
+    @GetMapping(value = PATH)
+    public ResponseEntity<FizzBuzzResponse> getFizzBuzz() {
+        return new ResponseEntity<>(FizzBuzzResponse.builder()
+                .result("Health").build(), HttpStatus.OK);
+    }
 
     @PostMapping(value = PATH)
     public ResponseEntity<FizzBuzzResponse> postFizzBuzz(@RequestBody FizzBuzzRequest body) {
